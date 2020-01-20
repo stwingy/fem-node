fb68e7209a0d9d993dc3e0a641cd2dbdc452b4e4
stwingy <john@scallywag.org.uk>
2020-01-20 15:39:16 +0000

    configured overwrite function for models not adhering to rest

--- FEMM/controllers.js
+++ FEMM/controllers.js
@@ -1,3 +1,8 @@
 const { crudControllers } = require('./control');
 const model = require('./model');
-module.exports = crudControllers(model);
+module.exports = crudControllers(model)
+
+
+
+//overwrite removeOne
+//module.exports = {...crudControllers(model),removeOne(req,res){res.send("NO DELETE")}}
