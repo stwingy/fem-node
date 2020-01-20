1610075f7a503b64cf4f3174b39ed19979c83319
John lawley <sywingy@gmail.com>
2020-01-20 08:54:59 +0000

    finished basic crud

--- models.js
+++ models.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
 const connect = (url, opts = {}) => {
-	return mongoose.connect(url, { ...opts, useNewUrlParser: true, useUnifiedTopology: true });
+	return mongoose.connect(url, { ...opts, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 };
+let db = mongoose.connection;
 
 const listSchema = new mongoose.Schema({
 	name: {
@@ -15,20 +15,27 @@ const listSchema = new mongoose.Schema({
 	count: Number
 });
 const listModel = mongoose.model('list', listSchema);
+
 const run = async () => {
-	await connect('mongodb://localhost:27017/api-test');
-	const list = await listModel.create({ name: 'first', description: 'start', count: 1 }); //create
-	console.log(list);
-	console.log(await list.findById(list._id).exec()); //read  exec turns into real promise
-	const updated = await list
+	await connect('mongodb://localhost:27017/api-testDB');
+	console.log('connected');
+	const list = await listModel.create({ name: 'second', description: 'start3', count: 3 });
+
+	console.log('CREATE ', list);
+	console.log('READALL ', await listModel.find({}).exec());
+	console.log('READ ', await listModel.findById(list._id).exec());
+	const updated = await listModel
 		.findByIdAndUpdate(
 			list._id,
-			{ name: 'initial' },
+			{ name: 'rubbish' },
 			{ new: true } //so it returns new values     update
 		)
 		.exec();
-	console.log(updated);
-	const deleted = await list.findByIdAndRemove(list._id).exec();
+	console.log('UPDATE ', updated);
+	const deleted = await listModel.findByIdAndRemove(list._id).exec();
+	console.log('DELETE ', deleted);
+	db.close();
+	console.log('disconnected');
 };
 run();
 //mongoose.Types.ObjectId  -fake reference
